62b1d0155a0e61309cebd59fe1d29fac12fbbc9f
Janik Knittle <janik.knittle@gmail.com>
2016-07-08 19:57:08 -0700

    Add styling, modal and blurring

---
+++ app/components/book-cover.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  actions: {
+
+    open() {
+      this.set('isShowingModal', true);
+      this.get('blurBackground')(true);
+    },
+
+    close() {
+      this.set('isShowingModal', false);
+      this.get('blurBackground')(false);
+    }
+  }
+
+
+});
